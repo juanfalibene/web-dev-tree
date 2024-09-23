6ca5d563ffae0cbb4ef16bc0384d1788973cc769
juanfalibene <juanignaciofalibene@gmail.com>
2024-09-23 11:49:11 +0200

    separate footer content

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+<footer>
+        <a
+          href="https://juanfalibene.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src="http://localhost:3000/wp-content/themes/webdevtree/images/juanfalibene_profile.svg"
+            class="img-profile"
+          />juanfalibene.com</a
+        >
+      </footer>
+    </main>
+    <?php wp_footer(); ?>
+  </body>
+</html>
