dcdd60cee43fe5c1954601b403c07fa70cbe742e
juanfalibene <juanignaciofalibene@gmail.com>
2024-09-24 14:33:00 +0200

    Node List created from Custom Post Resources

--- index.php
+++ index.php
@@ -23,109 +23,70 @@
             $cat_slug = $category->slug;
             $cat_id = $category->term_id;
 
+            // Categories List
             echo '<li class="category" id="' . esc_attr( $cat_slug) . '">';
             echo '<a href="#">'. esc_html( $cat_name ) .'</a>';
             echo '</li>';
 
-            endforeach;
-          ?>
+            // Resource Post Elements
+            $query_args = [
+              'post_type' => 'resource',
+              'tax_query' => [
+                [
+                'taxonomy' => 'category_resource',
+                'field' => 'slug',
+                'terms' => $cat_slug // Category Filter
+              ],
+            ],
+          ];
 
-        </ul>
+        endforeach;
 
+        echo '</ul>';
+        echo '<ul class="tree" id="resources-list">';
+          // Resources Node Tree
 
-        <!-- Resources Post Tree -->
-        <ul class="tree">
-          <li class="node docs active">
-            <a href="html/index.html" rel="noopener noreferrer" target="_blank"
-              >Docs 01<span>Description</span></a
-            >
-          </li>
-          <li class="node docs">
-            <a href="html/index.html" rel="noopener noreferrer" target="_blank"
-              >Docs 02<span>Description</span></a
-            >
-          </li>
-          <li class="node inspiration">
-            <a href="css/index.html" rel="noopener noreferrer" target="_blank"
-              >Ins 01</a
-            >
-          </li>
-          <li class="node voices">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Voices 01</a
-            >
-          </li>
-          <li class="node news">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >News 01</a
-            >
-          </li>
-          <li class="node tools">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Tools 01</a
-            >
-          </li>
-          <li class="node ui">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >UI 01</a
-            >
-          </li>
-          <li class="node ux">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >UX 01</a
-            >
-          </li>
-          <li class="node design">
-            <a href="javascript/index.html">Design 01</a>
-          </li>
-          <li class="node code">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Code 01</a
-            >
-          </li>
-          <li class="node tutorials">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Tutorials 01</a
-            >
-          </li>
-          <li class="node newsletters">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Newsletters 01</a
-            >
-          </li>
-          <li class="node playlists">
-            <a
-              href="javascript/index.html"
-              rel="noopener noreferrer"
-              target="_blank"
-              >Playlists 01</a
-            >
-          </li>
-        </ul>
+          foreach ( $categories as $category ) :
+            $cat_slug = $category->slug;
+
+            $query_args = [
+              'post_type' => 'resource',
+              'tax_query' => [
+                  [
+                      'taxonomy' => 'category_resource',
+                      'field'    => 'slug',
+                      'terms'    => $cat_slug,
+                  ],
+              ],
+          ];
+
+          $query_post = new WP_Query( $query_args );
+
+          if ( $query_post->have_posts() ) :
+
+            while ( $query_post->have_posts() ) : $query_post->the_post();
+            // Custom Post Field: external_link
+            $external_link = get_post_meta(get_the_ID(), '_external_link', true);
+
+            // Print Resource Node
+            echo '<li class="node ' . esc_attr($cat_slug) . '">';
+            echo '<a href="'. esc_url($external_link) .'" rel="noopener noreferrer" target="_blank">';
+            echo get_the_title() . '<span>' . get_the_excerpt() . '</span>';
+            echo '</a>';
+            echo '</li>';
+
+          endwhile;
+
+        endif;
+
+        wp_reset_postdata();
+
+      endforeach;
+      
+      echo '</ul>';
+
+      ?>
+
       </section>
       <?php
         get_template_part('templates/footer');
