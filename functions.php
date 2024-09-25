aec4d4510d87a27f969383635ddc74b44df7314c
juanfalibene <juanignaciofalibene@gmail.com>
2024-09-25 18:07:11 +0200

    register sidebar for archive content template

--- functions.php
+++ functions.php
@@ -60,6 +60,15 @@ function webdevtree_setup_widgets()
         'before_title' => '<h2 class="widget-title">',
         'after_title' => '</h2>',
     ]);
+
+    register_sidebar([
+        'name' => 'Archive Page Sidebar',
+        'id' => 'archive-sidebar',
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget' => '</section>',
+        'before_title' => '<h2 class="widget-title">',
+        'after_title' => '</h2>',
+    ]);
 }
 
 add_action('widgets_init', 'webdevtree_setup_widgets');
