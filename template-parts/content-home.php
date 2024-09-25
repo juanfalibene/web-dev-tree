a90873c43d986466fa70a73ae90a3f3fbd884cb1
juanfalibene <juanignaciofalibene@gmail.com>
2024-09-25 16:57:37 +0200

    template base for loop posts created

---
+++ template-parts/content-home.php
@@ -0,0 +1,22 @@
+<section class="blog-section">
+    <div class="blog-post-list">
+        <?php
+        if (have_posts()):
+            while (have_posts()):
+                the_post();
+                get_template_part('template-parts/content', get_post_type());
+            endwhile;
+            get_template_part('template-parts/pagination');
+        else:
+            get_template_part('template-parts/loop-error');
+        endif;
+        ?>
+    </div>
+    <sidebar class="blog-post-sidebar">
+        <?php
+        if (is_active_sidebar('home-sidebar')):
+            dynamic_sidebar('home-sidebar');
+        endif;
+        ?>
+    </sidebar>
+</section>
