7bd6de242d9efa37c2ca289768ac072241cadd66
juanfalibene <juanignaciofalibene@gmail.com>
2024-09-25 16:49:47 +0200

    better class name added

--- footer.php
+++ footer.php
@@ -1,16 +1,10 @@
-<footer>
-        <a
-          href="https://juanfalibene.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="http://localhost:3000/wp-content/themes/webdevtree/images/juanfalibene_profile.svg"
-            class="img-profile"
-          />juanfalibene.com</a
-        >
-      </footer>
-    </main>
-    <?php wp_footer(); ?>
-  </body>
+<footer class="main-footer">
+  <a href="https://juanfalibene.com" target="_blank" rel="noopener noreferrer">
+    <img src="http://localhost:3000/wp-content/themes/webdevtree/images/juanfalibene_profile.svg"
+      class="img-profile" />juanfalibene.com</a>
+</footer>
+</main>
+<?php wp_footer(); ?>
+</body>
+
 </html>
