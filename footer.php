8bf5cc33fdbef5c3df1f146d6395b32fea757dde
juanfalibene <juanignaciofalibene@gmail.com>
2024-10-01 18:10:43 +0200

    custom settings added

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <footer class="main-footer">
-  <a href="https://juanfalibene.com" target="_blank" rel="noopener noreferrer">
-    <img src="http://localhost:3000/wp-content/themes/webdevtree/images/juanfalibene_profile.svg"
-      class="img-profile" />juanfalibene.com</a>
+  <a href="<?php echo esc_url(get_theme_mod('footer_link', 'https://juanfalibene.com')); ?>" target="_blank" rel="noopener noreferrer">
+    <img src="<?php echo esc_url(get_theme_mod('footer_image', get_template_directory_uri() . '/images/juanfalibene_profile.svg')); ?>" class="img-profile" />
+    <?php echo esc_html(get_theme_mod('footer_link_name', 'juanfalibene.com')); ?>
+  </a>
 </footer>
 </main>
 <?php wp_footer(); ?>
 </body>
-
 </html>
