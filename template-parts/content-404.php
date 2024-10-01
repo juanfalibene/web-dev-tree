0bb6e868d4c25d36936916b89827944d8c711552
juanfalibene <juanignaciofalibene@gmail.com>
2024-10-01 18:07:20 +0200

    404 page template created

---
+++ template-parts/content-404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<section class="single-post">
+    <article class="single-post-content">
+        <h2><?php _e('Page Not Found', 'web-dev-tree'); ?></h2>
+        <p><?php _e('The page you were looking for could not be found.', 'web-dev-tree'); ?></p>
+    </article>
+</section>
+
+<?php get_footer(); ?>
