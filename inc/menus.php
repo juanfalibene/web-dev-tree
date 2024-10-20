eb8d972b8c22bdd030a6f407a6786da5c3610c1f
Juan Ignacio Falibene <juanignaciofalibene@gmail.com>
2024-10-20 13:34:23 +0200

    delete footer menu register

--- inc/menus.php
+++ inc/menus.php
@@ -3,6 +3,5 @@
 // Register menus
 function webdevtree_register_menus() {
     register_nav_menu( 'main-menu', __( 'Main Menu', 'webdevtree') );
-    /*register_nav_menu( 'footer-menu', __( 'Footer Menu', 'webdevtree') );*/
 }
 add_action('after_setup_theme', 'webdevtree_register_menus');
